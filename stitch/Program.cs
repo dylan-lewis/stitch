607b5a3668bf535f618f419b96375f83c2fa80cd
Dylan Lewis <dylan.lewis@internode.on.net>
2016-05-08 11:39:18 +1000

    Removed display of full path when printing individual file names

--- stitch/Program.cs
+++ stitch/Program.cs
@@ -2,85 +2,81 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace stitch
 {
     public class Program
     {
+        private const string FileType = ".csv";
+        private static readonly string Cr = Environment.NewLine;
+        private static readonly string Dir = Directory.GetCurrentDirectory();
+
         public static void Main(string[] args)
         {
-            const string dir = @"C:\temp\";  //Directory.GetCurrentDirectory();
-            const string fileType = "*.csv";
             string header = null;
-
-            Console.WriteLine($@"Working in directory {dir}");
-
+            var filePaths = Directory.GetFiles(Dir, "*"+ FileType);
+            var fileNames = new List<string>();
             try
             {
-                string[] filePaths = Directory.GetFiles(dir, fileType);
-                int fileCount = filePaths.Length;
+                var fileCount = filePaths.Length;
+                var data = new List<string>();
 
-                List<string> data = new List<string>();
+                Console.WriteLine($"{Cr}Processing {filePaths.Length} file{PrintSIfPlural(filePaths.Length)} in {Dir}:");
 
-                Console.WriteLine($"\nProcessing {fileCount} file" +
-                                  ((fileCount != 1) ? "s:" : ":"));
-
-                foreach (string filePath in filePaths)
+                foreach (var filePath in filePaths)
                 {
+                    //remove path data, add to fileName list, and display file name
+                    var fileName = GetFileNameFromPath(filePath);
+                    fileNames.Add((fileName));
+                    Console.Write($"\t{fileName}... ");
 
-                    Console.Write($"\t {filePath}... ");
+                    //read file, ensure data exists
+                    var content = File.ReadAllLines(filePath).ToList();
+                    if (content.Count == 0)
+                        throw new InvalidDataException("No data in file");
 
-                    //read file and check header consistency
-                    List<string> content = File.ReadAllLines(filePath).ToList();
+                    //check header consistency then add lines to data
                     if (header == null)
                     {
                         header = content[0];
                     }
-                    else if (header != content[0])
+                    else if (!header.Equals(content[0]))
                     {
-                        const string error = "Inconsistant data headings.";
-                        throw new NotSupportedException(error);
+                        throw new InvalidDataException("Inconsistant data headings");
                     }
                     else
                     {
-                        content.RemoveAt(0); //as already exists
+                        content.RemoveAt(0); //as data headings already exists
                     }
 
-                    //TODO check for duplicates?
-
+                    //TODO check for duplicates lines?
+                    Console.Write($"{content.Count} row{PrintSIfPlural(content.Count)}\t");
                     data.AddRange(content);
-                    Console.WriteLine($" { content.Count} rows.\tOK");
+                    Console.WriteLine("OK");
 
                 }
 
-                Console.WriteLine($"Writing header (1 row) and data ({data.Count} " + 
-                    ((data.Count == 1) ? "row" : "rows") + ").");
-
-                Console.WriteLine($"Output to {WriteFile(data)}");
-
-                Console.WriteLine("\nSuccess.");
+                Console.Write($"{Cr}Output (1 header row and {data.Count} data row{PrintSIfPlural(data.Count)}) to file: ");
+                Console.WriteLine($"{WriteFile(data, DetermineOutputFileName(fileNames))}");
+                Console.WriteLine("Success.");
             }
-
-
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message + Environment.NewLine + "Processing cancelled.");
-            }
-            finally
-            {
-                Console.ReadLine();
+                Console.WriteLine($"{Cr}\t{ex.Message}. {Cr}Please check input data file before trying again{Cr}File stitching cancelled");
+                Console.ReadKey();
             }
         }
 
-        public static string WriteFile(List<string> lines, string fileName = "output")
+
+        private static string WriteFile(List<string> lines, string fileName)
         {
-            string outExtension = ".csv";
-            string outFolder = Environment.GetFolderPath(
+            var outFolder = Environment.GetFolderPath(
                 Environment.SpecialFolder.Desktop
-                );
-            string outTimeStamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
-            string outPath = outFolder + Path.DirectorySeparatorChar + 
-                        fileName + outTimeStamp + outExtension;
+                ) 
+                + Path.DirectorySeparatorChar;
+            var timeStamp = DateTime.Now.ToString("yyyy-MM-dd_HHmmss");
+            var outPath = outFolder + fileName + timeStamp + FileType;
 
             if (!File.Exists(outPath))
             {
@@ -90,5 +86,54 @@ namespace stitch
             return outPath;
         }
 
+        private static string GetFileNameFromPath(string filePath)
+        {
+            var split = filePath.LastIndexOf(Path.DirectorySeparatorChar);
+            return filePath.Substring(split + 1);
+        }
+
+
+        private static string DetermineOutputFileName(IEnumerable<string> fileNames)
+        {
+            const string defaultName = "stitched_";
+            var newName = string.Empty;
+
+            foreach (var fileName in fileNames)
+            {
+
+                if (newName.Equals((string.Empty)))
+                {
+                    newName = fileName;
+                }
+                else if (!fileName.StartsWith(newName))
+                {
+                    //determine minimum string length; iterate and break on difference
+                    var count = newName.Length < fileName.Length ? newName.Length : fileName.Length;
+                    for (var i = 0; i < count; i++)
+                    {
+                        if (!string.Equals(newName[i], fileName[i]))
+                        {
+                            newName = newName.Substring(0, i);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return newName.Length > 0 ? newName + "_" : defaultName;
+        }
+
+        private static string PrintSIfPlural(int count)
+        {
+            return count == 1 ? string.Empty : "s";
+        }
+    }
+
+
+    public class InvalidDataException : Exception
+    {
+        public InvalidDataException(string message)
+            : base(message)
+        {}
     }
 }
